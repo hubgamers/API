a021a30d94a9940e74ec2f7e0dafd519b25a091d
Alexis BRIET <alexis.briet2003@gmail.com>
2024-03-01 19:11:43 +0100

    First commit

---
+++ src/main/java/com/hubgamers/api/HubgamersApplication.java
@@ -0,0 +1,13 @@
+package com.hubgamers.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubgamersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubgamersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hubgamers/api/HubgamersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubgamers.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubgamersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
