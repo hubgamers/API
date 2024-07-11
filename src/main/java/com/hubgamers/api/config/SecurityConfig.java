fb5e332e979397d276cb49acc3cc092339941d35
Alexis BRIET <alexis.briet2003@gmail.com>
2024-07-11 10:38:52 +0200

    Add cors

--- src/main/java/com/hubgamers/api/config/SecurityConfig.java
+++ src/main/java/com/hubgamers/api/config/SecurityConfig.java
@@ -19,6 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -59,6 +64,17 @@ public class SecurityConfig {
 				.httpBasic(Customizer.withDefaults())
 				.addFilterAfter(jwtAuthFilter, BasicAuthenticationFilter.class)
 				.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4173", "https://api-hubgamers.alexisbriet.fr", "https://hubgamers.alexisbriet.fr"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 	
 	@Bean
